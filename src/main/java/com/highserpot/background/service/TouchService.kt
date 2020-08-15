e7b7fc2d6e7a72371d9e2fd52054488b8a936503
cheolgyu <rb7480@knou.ac.kr>
2020-08-15 15:43:09 +0900

    1.0.1 더빠르게 터치

--- src/main/java/com/highserpot/background/service/TouchService.kt
+++ src/main/java/com/highserpot/background/service/TouchService.kt
@@ -18,7 +18,7 @@ class TouchService : AccessibilityService() {
         val clickPath = Path()
         clickPath.moveTo(x, y)
         val gestureBuilder = GestureDescription.Builder()
-        gestureBuilder.addStroke(StrokeDescription(clickPath, 0, 300))
+        gestureBuilder.addStroke(StrokeDescription(clickPath, 0, 100))
         dispatchGesture(gestureBuilder.build(), null, null)
     }
 
