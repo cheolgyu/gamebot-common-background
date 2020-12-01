6cf56f05423117d3bbb1b226139c650f8d5b4472
cheolgyu <rb7480@knou.ac.kr>
2020-12-01 17:44:09 +0900

    샘플은 풀스크린 모드로

--- src/main/java/com/highserpot/background/SampleActivity.kt
+++ src/main/java/com/highserpot/background/SampleActivity.kt
@@ -4,11 +4,14 @@ import android.content.pm.ActivityInfo
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Point
+import android.os.Build
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import java.io.File
 import java.io.InputStream
@@ -20,11 +23,35 @@ class SampleActivity : AppCompatActivity() {
 
     var mWidth = 0
     var mHeight = 0
+    private fun setFullscreen(fullscreen: Boolean) {
+        val actionBar: ActionBar? = supportActionBar
+        if (fullscreen) {
+            if (actionBar != null) actionBar.hide()
+            var flag: Int = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                flag = flag or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                flag = flag or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                flag = flag or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            }
+            window.decorView.systemUiVisibility = flag
+        } else {
+            if (actionBar != null) actionBar.show()
+            var flag: Int = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                flag = flag or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            }
+            window.decorView.systemUiVisibility = flag
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         setContentView(R.layout.activity_sample)
+        setFullscreen(true)
         Log.d(TAG, TAG.toString())
 
 
